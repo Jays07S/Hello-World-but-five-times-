fc5b77954a9b09f08f35c0bad61f9765daf2fa08
Jays07S <91245762+Jays07S@users.noreply.github.com>
2021-09-23 02:58:54 -0400

    Add files via upload

---
+++ Hello World 2.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : Hello.cpp
+// Author      : Justin Dejesus
+// Description : Hello World but printed five times.
+//============================================================================
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	for ( int i = 0; i < 5; ++i )
+	cout << "!!!Hello World\n" ;
+	return 0;
+}
